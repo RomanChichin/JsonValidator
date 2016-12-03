178c4ad3a58419848cfb252e508e92468ca67573
Roman Chichin <chichinroman.rf@gmail.com>
2016-12-03 19:36:05 +0300

    first commit

---
+++ JsonAnalyzer/JsonAnalyzer.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace JsonAnalyzer
+{
+    public class JsonAnalyzer
+    {
+
+        /// <summary>
+        /// Проверка JSON текста на валидность.
+        /// </summary>
+        /// <param name="jsonString"></param>
+        /// <returns></returns>
+        public static bool IsValidJson(string jsonString)
+        {
+            //Проверка входных данных.
+            if (jsonString == null || jsonString == string.Empty)
+            {
+                return false;
+            }
+
+            //Если строка начинается на { и заканчивается на }, то начать проверку
+            //в противном случае в файле ошибка.
+            if (jsonString[0] == '{'
+             && jsonString[jsonString.Length - 1] == '}')
+            {
+                // Объявляем объект класса Regex, который будет работать с шаблонами.
+                Regex regex;
+                //Количество раз которое будет произведена итеративная проверка.
+                byte trustNumberOfInspections = 10;
+
+                //Убираем из файла все лишнее(переносы строки, пробелы итд)
+                var analyzedText = jsonString.Replace(" ", string.Empty)
+                                             .Replace("\n", string.Empty)
+                                             .Replace("\r", string.Empty);
+
+                //Шаблоны, которые могут встетиться в json файле.
+                var basePattern = "\"(\\w|@|\\.|:|;|/|\\-|\\+|=|\\?|\\*|\\^|\\$|!|`|'|&|<|>|,|%|#|\\(|\\))+\"";
+
+                var bracketsCheckPattern = "(\\{\\},)*(\\{\\}){1}";
+
+                var commaCheckPattern = "((" + "\"(\\w|@|\\.|;|/|\\-|\\+|=|\\?|\\*|\\^|\\$|!|`|'|&|<|>|,|%|#|\\(|\\))+\"" 
+                    + "|false|true|null|\\]|\\}|:(\\d|\\.)+|:\"\")" + "(" + basePattern + "|\\{|\\[))" 
+                    + "|(,(\\}|\\]))";
+
+                string[] simplePatterns = {
+                       basePattern+":"+basePattern+",?",
+                       basePattern+":"+"(\\d|\\.)+" + ",?",
+                       basePattern+":"+"(true|false|null|\"\")" + ",?",
+                       "null,?"
+                    };
+
+                string[] compositePatterns = {
+                       basePattern + ":" + "\\[" + "("+basePattern + ",)*" + "("+basePattern + ")?" + "\\]" + ",?",
+                       basePattern + ":" + "\\[(\\{\\},)*(\\{\\}){1}\\]" + ",?",
+                       basePattern + ":" + "(\\[\\]|\\{\\})" + ",?",
+                       basePattern + ":" + "\\{(\\{\\},)*(\\{\\}){1}\\}" + ",?",
+                     };
+
+                //Проверяем правильность расстановки запятых в строке
+                if (Regex.IsMatch(analyzedText, commaCheckPattern))
+                {
+                    return false;
+                }
+
+                //Убираем из строки все элементы, которые соответствуют простым шаблонам
+                foreach (var pattern in simplePatterns)
+                {
+                    regex = new Regex(pattern);
+                    analyzedText = regex.Replace(analyzedText, string.Empty);
+                }
+
+                //Убираем из строки все элементы, которые соответствуют составным шаблонам
+                for (int i = 0; i < trustNumberOfInspections; i++)
+                {
+                    foreach (var pattern in compositePatterns)
+                    {
+                        regex = new Regex(pattern);
+                        analyzedText = regex.Replace(analyzedText, string.Empty);
+                    }
+                }
+
+                //Убираем из строки скобки, которые могли остаться в случае, если было несколько объектов
+                regex = new Regex(bracketsCheckPattern);
+                analyzedText = regex.Replace(analyzedText, string.Empty);
+
+                //Если на выходе строка пуста или состоит только из {}, значит файл прошел проверку на валидность.
+                if (analyzedText == "{}" || analyzedText == string.Empty)
+                {
+                    return true;
+                }
+                //Если остались какие-то еще символы, значит в файле есть ошибка.
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ JsonValidator_v2.0/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JsonValidator_v2._0
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ParseJSON(object sender, TextChangedEventArgs e)
+        {
+            if (textbox.Text == null || textbox.Text == string.Empty)
+            {
+                layoutGrid.Background = new SolidColorBrush(Colors.White);
+                textblock.Background = new SolidColorBrush(Colors.White);
+                textblock.Text = "Input JSON to check it.";
+
+            }
+            else if (JsonAnalyzer.JsonAnalyzer.IsValidJson(textbox.Text))
+            {
+                layoutGrid.Background = new SolidColorBrush(Colors.LightGreen);
+                textblock.Background = new SolidColorBrush(Colors.LightGreen);
+                textblock.Text = "Success! It`s correct.";
+            }
+            else
+            {
+                layoutGrid.Background = new SolidColorBrush(Colors.IndianRed);
+                textblock.Background = new SolidColorBrush(Colors.IndianRed);
+                textblock.Text = "There is a mistake here.";
+            }
+        }
+    }
+}
